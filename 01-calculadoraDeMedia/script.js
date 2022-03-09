a7f38223d1d4e2f41d111c5e86f913ce45dab694
Erika Freitas <erikamsfreitas@gmail.com>
2022-03-09 16:15:45 -0300

    Add the first project

---
+++ 01-calculadoraDeMedia/script.js
@@ -0,0 +1,22 @@
+function Calcular() {
+  var nota1 = parseFloat(document.getElementById("nota1").value);
+  var nota2 = parseFloat(document.getElementById("nota2").value);
+  var nota3 = parseFloat(document.getElementById("nota3").value);
+  var nota4 = parseFloat(document.getElementById("nota4").value);
+  var resultado = null;
+  var notaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+  var notaFixada = notaFinal.toFixed(1);
+
+  if (notaFixada >= 6) {
+    resultado = "Parabéns, você foi aprovade! &#128526";
+    document.getElementById('result').style.backgroundImage="url('https://c.tenor.com/kE2G0EC_4f0AAAAj/stitch-happy-new-year.gif')";
+  } else {
+    resultado = "Infelizmente você foi reprovade &#128532";
+    document.getElementById('result').style.backgroundImage="url('https://c.tenor.com/T1Sjs8hXXLsAAAAj/stitch-cry.gif')";
+  }
+
+  document.getElementById("result").innerHTML =
+    "Sua nota final é " + notaFixada + ". " + resultado;
+}
+
+// resultado = document.getElementById('result').innerHTML = "Atenção! Você precisa preencher todos os campos."
