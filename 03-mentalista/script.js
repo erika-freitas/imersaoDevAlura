6c395fdd239f2e0f07b4b2f55db6ac9870f044a5
Erika Freitas <erikamsfreitas@gmail.com>
2022-04-04 15:21:54 -0300

    Modify results display and add images

--- 03-mentalista/script.js
+++ 03-mentalista/script.js
@@ -4,16 +4,23 @@ function guessTheNumber() {
   let result = document.getElementById("result");
   let guess = parseInt(document.getElementById("valor").value);
   let alert = document.getElementById("alert");
+  let image = document.getElementsByClassName("container__image")[0];
   if (guess == secretNumber){
-    result.innerHTML = "Você acertou"
-    // add img
-    alert.innerHTML = ""
+    result.innerHTML = "Parabéns, você acertou!"
+    result.style.display = "inline";
+    alert.style.display = "none";
+    image.src = "https://imgur.com/8UzOOkx.png"
   } else if (guess > 10 || guess < 0) {
     alert.innerHTML = "Você deve digitar um número de 0 a 10"
-    result.innerHTML = ""
+    alert.style.display = "inline";
+    result.style.display = "none";
+    image.src = "https://i.kym-cdn.com/photos/images/newsfeed/001/318/550/e2d.jpg"
   } else {
-    result.innerHTML = "Errou"
-    alert.innerHTML = ""
-    // add img
+    result.innerHTML = "Errou, tente novamente!"
+    result.style.display = "inline";
+    alert.style.display = "none";
+    image.src = "https://imgur.com/KASnxm9.png";
   }
 }
+
+// #TODO: add guesses?
