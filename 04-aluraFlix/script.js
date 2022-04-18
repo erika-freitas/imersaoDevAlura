f011bfea47609ab9a7fd0ae2028108123eb88d0b
Erika Freitas <erikamsfreitas@gmail.com>
2022-04-18 10:20:42 -0300

    Aluraflix initial commit

---
+++ 04-aluraFlix/script.js
@@ -0,0 +1,11 @@
+var listaFilmes = [
+  "https://movieposters2.com/images/1177215-b.jpg",
+  "https://www.dvdplanetstore.pk/wp-content/uploads/2017/12/gUex3Twn97Jp8F27Op1r7OnMTFt.jpg",
+  "https://m.media-amazon.com/images/M/MV5BZWFjYmZmZGQtYzg4YS00ZGE5LTgwYzAtZmQwZjQ2NDliMGVmXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_FMjpg_UX1000_.jpg",
+  "https://static.wikia.nocookie.net/marveldatabase/images/e/ef/Spider-Man_Into_the_Spider-Verse_poster_003.jpg"
+]
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
+
